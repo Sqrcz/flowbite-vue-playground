b3a2697a8010155586a058fb4a8290d10c112e1b
Sqrcz <naorniakowski@slashlab.pl>
2024-04-25 16:17:17 +0200

    component wrapper added for easier testing

---
+++ src/components/ComponentWrapper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-6 last:mb-0">
+
+    <div class="relative mb-4">
+      <div class="absolute inset-0 flex items-center">
+        <div class="w-full border-t border-gray-300" />
+      </div>
+      <div class="relative flex justify-center">
+        <span class="bg-white px-3 text-base font-semibold leading-6 text-gray-900">{{label}}</span>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+})
+</script>
